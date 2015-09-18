17e02ee7f2752008d7c03321d73715c6fd4542b0
Tero Hänninen <lgvz@users.noreply.github.com>
2015-09-18 12:33:35 +0300

    Update to 2.0.0

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['imagefmt'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='imagefmt/enum.Error.html' title='imagefmt::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
